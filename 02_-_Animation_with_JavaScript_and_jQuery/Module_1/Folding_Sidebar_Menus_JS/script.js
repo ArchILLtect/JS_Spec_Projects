cef1c5e9afb632b715b9cf1997e38abd1ef6e7cc
ArchILLtect <nickhansonsr@gmail.com>
2023-09-09 10:58:03 -0500

    Complete Module 1 - Project 1 - Part 1

---
+++ 02_-_Animation_with_JavaScript_and_jQuery/Module_1/Folding_Sidebar_Menus_JS/script.js
@@ -0,0 +1,48 @@
+// Created on 9/08/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery"
+
+		
+const submenus = document.querySelectorAll('ul li ul');
+
+function openMenu() {
+    const menuItems = document.querySelectorAll('.menulink');
+
+    for ( menuItem of menuItems ) {
+        menuItem.addEventListener('click', function( event ) {
+            event.preventDefault();
+            let selectedMenu = event.target.parentNode.querySelector('ul');
+            menuVisibliltyToggle( selectedMenu );
+        })
+
+    }
+    return;
+}
+openMenu()
+
+
+//Use thise function to only have ONE submenu open at a time:
+function menuVisibliltyToggle( currentSubmenu ) {
+
+    for ( submenu of submenus ) { submenu.classList.add('hide-menu'); }
+    currentSubmenu.classList.remove('hide-menu');
+    return;
+}
+for ( submenu of submenus ) { submenu.classList.add('hide-menu'); }
+//End of single sub-menu opening functionality.
+
+
+/*
+// Use this function to leave the menus open while opening others:
+function menuVisibliltyToggle( currentSubmenu ) {
+
+    if ( currentSubmenu === undefined ) {
+        for ( submenu of submenus ) { submenu.classList.toggle('hide-menu'); }
+        return;
+    } else {
+        currentSubmenu.classList.toggle('hide-menu');
+        return;
+    }
+}
+menuVisibliltyToggle()
+//End of sub-menu left open functionality.
+*/
+
