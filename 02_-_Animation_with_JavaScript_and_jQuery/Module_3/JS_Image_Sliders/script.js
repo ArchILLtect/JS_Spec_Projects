15068d0c614c370ef96c0a1fdb8b4286d4bfb313
ArchILLtect <nickhansonsr@gmail.com>
2023-09-11 10:23:11 -0500

    Complete Course 2 - Module 3

--- 02_-_Animation_with_JavaScript_and_jQuery/Module_3/JS_Image_Sliders/script.js
+++ 02_-_Animation_with_JavaScript_and_jQuery/Module_3/JS_Image_Sliders/script.js
@@ -1,1 +1,37 @@
 // Created on 9/09/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery"
+
+window.addEventListener('load', function() {
+
+    const slideCount = document.querySelectorAll('#slider-wrapper ul li').length;
+    const slideWidth = document.querySelector('#slider-wrapper').offsetWidth;
+    const totalWidth = slideCount * slideWidth + 'px';
+    const slider = document.querySelector('#slider-wrapper ul');
+    const next = document.querySelector('#next');
+    const previous = document.querySelector('#prev');
+
+    console.log(slideWidth)
+
+    let leftPosition = 0;
+    let counter = 0;
+    slider.style.width = totalWidth;
+
+    next.addEventListener('click', function(event) {
+        event.preventDefault();
+        counter++;
+        if (counter == slideCount) { counter = 0; }
+        leftPosition = `-${counter * slideWidth}px`;
+        slider.style.left = leftPosition;
+    })
+
+    previous.addEventListener('click', function(event) {
+        event.preventDefault();
+        counter--;
+        if (counter < 0 ) { counter = slideCount - 1; }
+        leftPosition = `-${counter * slideWidth}px`;
+        slider.style.left = leftPosition;
+    })
+
+
+
+
+})
--- 02_-_Animation_with_JavaScript_and_jQuery/Module_3/jQuery_Image_Slider_Version_1/script.js
+++ 02_-_Animation_with_JavaScript_and_jQuery/Module_3/jQuery_Image_Slider_Version_1/script.js
@@ -1,2 +1,40 @@
 // Created on 9/09/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery"
 
+$(window).on('load', function() {
+    'use strict';
+
+    const imageCount = $('#slider ul li').length;
+    const imageWidth = $('#slider ul li img').first().width();
+    const totalWidth = (imageCount * imageWidth + 'px');
+
+
+    let leftPosition = 0;
+    let counter = 0;
+
+    $('#slider ul').css("width", totalWidth);
+
+    $('#next').on('click', function() {
+
+        counter++;
+
+        if ( counter == imageCount ) {
+            counter = 0;
+        }
+
+        leftPosition = `-${counter * imageWidth}px`;
+        $('#slider ul').animate( {left: leftPosition }, 700, 'easeInQuad' );
+    })
+
+    $('#previous').on('click', function() {
+
+        if ( counter == 0 ) {
+            counter = imageCount;
+        }
+
+        counter--;
+                
+        leftPosition = `-${counter * imageWidth}px`;
+        $('#slider ul').animate( {left: leftPosition }, 700, 'easeInQuad' );
+    })
+});
+
