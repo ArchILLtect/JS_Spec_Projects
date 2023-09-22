220c98d0835a818fe7651f7c8464723ae397399f
ArchILLtect <nickhansonsr@gmail.com>
2023-09-22 06:18:33 -0500

    Complete Course 3 Module 2 Part 1

--- 03_-_Interactivity_with_JavaScript_and_jQuery/Module_2/01_Smooth_Scroll_with_jQuery/smoothscroll.js
+++ 03_-_Interactivity_with_JavaScript_and_jQuery/Module_2/01_Smooth_Scroll_with_jQuery/smoothscroll.js
@@ -1,2 +1,15 @@
-// Smoothscroll Script...
+// Created on 9/09/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery"
 
+$('nav ul li a').on('click', function() {
+
+    let thisSection = $(this).attr('href');
+    let thisLink = $(this);
+
+    $('html, body').stop().animate( {
+        scrollTop: $(thisSection).offset().top -200 },
+        800, "easeOutCirc", function() {
+            $('nav ul li a').removeAttr('class');
+            $(thisLink).addClass("selected");
+            // alert( $(window).scrollTop() );
+        } );
+} );
--- 03_-_Interactivity_with_JavaScript_and_jQuery/Module_2/03_Scroll_Effects_with_JavaScript/script.js
+++ 03_-_Interactivity_with_JavaScript_and_jQuery/Module_2/03_Scroll_Effects_with_JavaScript/script.js
@@ -1,1 +1,2 @@
-// JavaScript goes here
+// Created on 9/09/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery"
+
