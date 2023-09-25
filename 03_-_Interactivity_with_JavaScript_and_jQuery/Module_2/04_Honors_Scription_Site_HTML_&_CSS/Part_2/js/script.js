1cdcac9ac19abc4be576b3789eda3b4fd74864a1
ArchILLtect <nickhansonsr@gmail.com>
2023-09-25 16:47:27 -0500

    Complete Course 3 Module 2 including Honors content

--- 03_-_Interactivity_with_JavaScript_and_jQuery/Module_2/04_Honors_Scription_Site_HTML_&_CSS/Part_2/js/script.js
+++ 03_-_Interactivity_with_JavaScript_and_jQuery/Module_2/04_Honors_Scription_Site_HTML_&_CSS/Part_2/js/script.js
@@ -1,14 +1,83 @@
 // Smooth Scroll
 
+(function() {
+
+    // NavBar
+    $('nav ul li a').on('click', function() {
+
+        let thisSection = $(this).attr('href');
+    
+        $('html, body').stop().animate( {
+            scrollTop: $(thisSection).offset().top -200 },
+            600, "easeOutCirc" );
+    
+    } );
+    
+    //Logo
+    $('.logo').on('click', function() {
+    
+        $('html, body').stop().animate( {
+            scrollTop: $('#page').offset().top -200 },
+            600, "easeOutCirc" );
+    
+    } );
+}());
 
 // Flexslider
+
+$(window).on('load', function() {
+    $('.flexslider').flexslider(
+        {
+            animation: "slide",
+            slideshowSpeed: 2000,
+            pauseOnHover: true,
+        }
+    );
+});
 
 
 // Tabs
 
+(function() {
+    $('#tabs > ul > li > a').on('click', function() {
+        $('#tabs > ul > li > a').css({ background: "var(--tea-green)", color: "black" });
+        $(this).css( { background: "var(--tea-green-light)", color: "black" } );
+        const thisTab = $(this).attr("href");
+        $('#tabs > div:visible').fadeOut(200, function() {
+            $(thisTab).fadeIn(200);
+        });
+    });
+}());
 
 // Content Rotator
 
+(function(){
+	"use strict";
+	
+	let counter = 1;
+
+	function contentRotator() {
+		$(`#rotator blockquote:nth-child(${counter})`).fadeIn(2000, function(){
+
+			if( $(this).is("#rotator blockquote:last-child") ) {
+				setTimeout( function() {
+					$(`#rotator blockquote:nth-child(${counter})`).fadeOut(2000, function() {
+						counter = 1;
+						contentRotator();
+					} );
+				}, 6000 );
+			} else {
+				setTimeout ( function() {
+					$(`#rotator blockquote:nth-child(${counter})`).fadeOut(2000, function() {
+						counter ++;
+						contentRotator();
+					});
+				}, 6000 );
+			}
+		});
+	};
+contentRotator();
+}());
 
 // Features Rotator 
 
