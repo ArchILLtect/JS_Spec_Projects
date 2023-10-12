ab0c542ba88e2f71da01c4db77fb1c75dfa6d472
ArchILLtect <nickhansonsr@gmail.com>
2023-10-12 16:47:36 -0500

    Complete Entire Specialization with only Honors material left

--- 04_-_Data_Manipulation_in_JavaScript/Module_4/04_Honors_Project-_Studio_Ghibli_Project_-_Third_Party_APIs/Part_1/script.js
+++ 04_-_Data_Manipulation_in_JavaScript/Module_4/04_Honors_Project-_Studio_Ghibli_Project_-_Third_Party_APIs/Part_1/script.js
@@ -1,2 +1,24 @@
 /* Created on 10/11/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery" */
 
+const mainElement = document.querySelector('main');
+async function getFilms() {
+ const filmsPromise = await fetch('https://ghibliapi.herokuapp.com/films');
+ const films = await filmsPromise.json();
+ console.log(films);
+}
+getFilms();
+
+function createCard(data) {
+    const card = document.createElement('article');
+    const movieTitle = document.createElement('h2');
+    const movieTitleTxt = document.createTextNode(data.title);
+    movieTitle.appendChild(movieTitleTxt);
+    const director = document.createElement('p');
+    const directorTxt = document.createTextNode(`Director: ${data.director}`);
+    director.appendChild(directorTxt);
+    //write similar code for year, description and score
+    card.appendChild(movieTitle);
+    card.appendChild(director);
+    // append the year, description and score
+    mainElement.appendChild(card);
+   }
