80ce4dfc9058590b7a89c7eb1c94f2def7e786d9
ArchILLtect <nickhansonsr@gmail.com>
2023-10-13 10:35:08 -0500

    Add Part 1 of the STAPI displayer

--- 04_-_Data_Manipulation_in_JavaScript/Module_4/04_Honors_Project-_Studio_Ghibli_Project_-_Third_Party_APIs/Part_1/script.js
+++ 04_-_Data_Manipulation_in_JavaScript/Module_4/04_Honors_Project-_Studio_Ghibli_Project_-_Third_Party_APIs/Part_1/script.js
@@ -10,15 +10,34 @@ getFilms();
 
 function createCard(data) {
     const card = document.createElement('article');
+
     const movieTitle = document.createElement('h2');
     const movieTitleTxt = document.createTextNode(data.title);
     movieTitle.appendChild(movieTitleTxt);
+
     const director = document.createElement('p');
     const directorTxt = document.createTextNode(`Director: ${data.director}`);
     director.appendChild(directorTxt);
+
     //write similar code for year, description and score
+    const year = document.createElement('p');
+    const yearTxt = document.createTextNode(`Released: ${data.release_date}`);
+    director.appendChild(directorTxt);
+
+    const description = document.createElement('p');
+    const descriptionTxt = document.createTextNode(data.description);
+    description.appendChild(descriptionTxt);
+
+    const rating = document.createElement('p');
+    const ratingTxt = document.createTextNode(`Rotten Tomatoes Score: ${data.rt_score}`);
+    rating.appendChild(ratingTxt);
+
     card.appendChild(movieTitle);
     card.appendChild(director);
     // append the year, description and score
+    card.appendChild(year);
+    card.appendChild(description);
+    card.appendChild(rating);
+
     mainElement.appendChild(card);
    }
